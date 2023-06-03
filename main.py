b4b4fabc5746b6a6367ef59c965e82113f233313
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-06-03 19:02:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -210,7 +210,8 @@ class AudioTool(BoxLayout):
         #input_file = 'testaudio.mp4'
         #output_file = 'output2.wav'
         #subprocess.run(['ffmpeg', '-i', input_file, '-acodec', 'pcm_s16le', '-ar', '44100', output_file])
-        self.outPutFile = self.storage_path + "/wav/output2.wav"
+        #self.outPutFile = self.storage_path + "/wav/output2.wav"
+        self.outPutFile  = "/data/data/org.example.c4k_tflite_audio1/files/app/wav/output2.wav"
         print("self.outPutFile")
         print(self.outPutFile)
         command2wav = "ffmpeg -i testaudio.mp4 " + self.outPutFile
@@ -244,7 +245,7 @@ class AudioTool(BoxLayout):
         
         #self.audio_path = dirCheck1 + "/wav/output1.wav"
         #self.audio_path = "/storage/emulated/0/org.example.c4k_tflite_audio1/wav/output1.wav"##Not found
-        self.audio_path = "/data/data/org.example.c4k_tflite_audio1/files/app/wav/output1.wav"
+        self.audio_path = "/data/data/org.example.c4k_tflite_audio1/files/app/wav/output2.wav"
         #self.audio_path = "wav/output2.wav"
      
         self.sound.setDataSource(self.audio_path) 
